7215e4a9ba72da3f3e3f54b9616e0d7dd6470d39
Lastdough <abdurraahm@gmail.com>
2022-06-27 23:06:30 +0700

    last commit

--- suhuProg.c
+++ suhuProg.c
@@ -1,3 +1,21 @@
 #include <stdio.h>
 
+int main()
+{
+    int suhu;
 
+    printf("Masukan suhu dalam celcius: ");
+    scanf("%i", &suhu);
+
+    if (suhu < 16){
+        printf("Dingin \n");
+    }
+    else if (suhu >= 16 && suhu <= 26){
+        printf("Sejuk \n");
+    }
+    else if (suhu > 26 ){
+        printf("Panas \n");
+    }
+
+    return 0;    
+}
